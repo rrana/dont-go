b01a8f9bd52982eff3e955e325fb3fe61d07862e
rrana <rana@air.inc>
2025-02-04 02:25:33 +0600

    Init 1

---
+++ 1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt" // library (package from standard go library)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("Hello Aspiring Go Developers")
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
+	}
+}
